990a133324518feb936d56297ac09d8230a4a0a8
maulaul <maulana.auliar@gmail.com>
2018-02-08 18:25:12 +0800

    finish easy 001

--- python/easy/challenge_001.py
+++ python/easy/challenge_001.py
@@ -1,16 +1,21 @@
-def writeToFile(name, age, username):
+def writeToFile(*args):
     #TODO: *save latest userdata from askUser() to a file, for later access.
-    pass
+    with open('challenge_001.txt', 'w') as f:
+        data = ",".join(list(args))
+        f.write(data)
 
 def askUser():
     name = input("what is your name: ")
     age = input("what is your age: ")
     uname = input("what is your uname: ")
 
-    print("your name is {name}, you are {age} years old, and your username is {uname}".format(
-        name=name, age=age, uname=uname
-    ))
+    #print("your name is {name}, you are {age} years old, and your username is {uname}".format(
+    #    name=name, age=age, uname=uname
+    #))
+    #using f-string on Python >= 3.6
+    print(f"your name is {name}, you are {age} years old, and your username is {uname}")
+    writeToFile(name, age, uname)
+    print("saved to file.")
 
 if __name__ == "__main__":
     askUser()
-    
