8c0674cd658ba33fed1113ce2e00efd15dc9ed24
maulaul <maulana.auliar@gmail.com>
2018-02-12 15:23:22 +0800

    init 002 ez

---
+++ python/easy/challenge_002.py
@@ -0,0 +1,18 @@
+def cal_force(m, a):
+    print(m*a)
+
+def help():
+    print('''syntax -> 'function'<space>'inputs' separated by spaces
+            example to calculate force of mass 100 and acceleration of 5.
+            force 100 5''')
+
+def cal():
+    fun = ['force', 'help','exit']
+    print('''Just to calculate
+            syntax -> 'function'<space>'inputs' separated by spaces''')
+    user_in = input('>').split(' ')
+    #TODO
+    #add dictionary function.
+    
+if __name__ == '__main__':
+    cal()
