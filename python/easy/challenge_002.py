17b1711b4d3efe7d2df3617ed398287a8014f2a0
maulaul <maulana.auliar@gmail.com>
2018-02-12 15:47:30 +0800

    add function dict

--- python/easy/challenge_002.py
+++ python/easy/challenge_002.py
@@ -1,19 +1,25 @@
 def cal_force(m, a):
     print(m*a)
 
-def help():
+def fun_help():
     print('''syntax -> 'function'<space>'inputs' separated by spaces
             example to calculate force of mass 100 and acceleration of 5.
             force 100 5''')
 
 def cal():
-    fun = ['force', 'help','exit']
+    func_dict = {
+                    'force':cal_force
+                    'help':fun_help
+    }
+    
     print('''Just to calculate
-            syntax -> 'function'<space>'inputs' separated by spaces''')
-    user_in = input('>').split(' ')
-    #TODO
-    #add dictionary function.
+            syntax -> 'function'<space>'inputs' separated by spaces
+            type 'help' for info. type 'exit' to close program. ''')
+    
+    while True:
+        user_in = input('>').split(' ')
+        if user_in[0] == 'exit':
+            break
     
 if __name__ == '__main__':
     cal()
-    
