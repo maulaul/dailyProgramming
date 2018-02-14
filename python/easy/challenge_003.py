af495f3661d39cc181046ad63ca0dd906804e57b
maulaul <maulana.auliar@gmail.com>
2018-02-14 18:53:25 +0800

    add

--- python/easy/challenge_003.py
+++ python/easy/challenge_003.py
@@ -1,9 +1,14 @@
 #ALPHABET CHIPERS.
+#TODO raise different error if encrypt/decrypt command not found.
 import sys
 
 def caesar():
     try:
-        print(sys.argv[1])
+        #print(sys.argv[1])
+        #print(sys.argv[2])
+        with open(sys.argv[1]) as f_in:
+            lines = list(f)
+
     except IndexError as e:
         print("Input file name cannot be empty.")
 
