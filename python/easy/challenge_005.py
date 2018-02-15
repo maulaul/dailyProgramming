368d2704cd599e4a75ea7750c04b667219dcb368
maulaul <maulana.auliar@gmail.com>
2018-02-15 21:08:52 +0800

    init 005

--- python/easy/challenge_005.py
+++ python/easy/challenge_005.py
@@ -3,3 +3,7 @@
 #TODO if user exist and password correct print something like program unlocked
 #TODO if user exist and password incorrect ask 2 more time before program off
 #TODO if user doesnt exit, ask continously.
+
+if __name__ == '__main__':
+    pass
+    
