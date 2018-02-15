3e3d9b34dfc0feb3cbb2a56f61da435132957228
maulaul <maulana.auliar@gmail.com>
2018-02-15 20:52:09 +0800

    add basic pass gen

--- python/easy/challenge_004.py
+++ python/easy/challenge_004.py
@@ -1,10 +1,25 @@
 import random, string
 
-PASS_CHOICE = string.ascii_letters + string.ascii_digits
+PASS_CHOICE = string.ascii_letters + string.digits
 
 def pass_gen(n):
-    pass
+    s = ''
+    for i in range(n):
+        s += random.choice(PASS_CHOICE)
+
+    return s
+
+def generate_n_pass(n, m):
+    pass_list = []
+    for i in range(n):
+        pass_list.append(pass_gen(m))
+
+    return pass_list
+
+def print_pass(pass_list):
+    for p in pass_list:
+        print(p)
 
 if __name__ == '__main__':
-    print(PASS_CHOICE)
+    print_pass(generate_n_pass(3, 12))
     
