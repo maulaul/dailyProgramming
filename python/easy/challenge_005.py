2a1bd17efadf5ba89dbf14c23a90a742818c1ed3
maulaul <maulana.auliar@gmail.com>
2018-02-15 21:08:23 +0800

    init 005

---
+++ python/easy/challenge_005.py
@@ -0,0 +1,5 @@
+#TODO read user.txt and pass.txt
+#TODO ask user to input username, password
+#TODO if user exist and password correct print something like program unlocked
+#TODO if user exist and password incorrect ask 2 more time before program off
+#TODO if user doesnt exit, ask continously.
