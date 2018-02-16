ab42eac41c724f77ceebad694b1f923131d8edc4
maulaul <maulana.auliar@gmail.com>
2018-02-16 23:43:06 +0800

    init func template 005

--- python/easy/challenge_005.py
+++ python/easy/challenge_005.py
@@ -3,7 +3,19 @@
 #TODO if user exist and password correct print something like program unlocked
 #TODO if user exist and password incorrect ask 2 more time before program off
 #TODO if user doesnt exit, ask continously.
+def read_db(filename):
+    pass
+
+def check_user(username, f):
+    pass
+
+def check_pass(username, f):
+    pass
+
+def aProgram():
+    #create program loop here.
+    pass
 
 if __name__ == '__main__':
-    pass
+    aProgram()
     
